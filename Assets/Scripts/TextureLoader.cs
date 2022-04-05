e3266cb861cb38f889eaead18aad9e0f360bf9f3
Aleksey-Golub <55555257+Aleksey-Golub@users.noreply.github.com>
2022-04-05 18:17:57 +0200

    fix
    Dispose UnityWebRequest

--- Assets/Scripts/TextureLoader.cs
+++ Assets/Scripts/TextureLoader.cs
@@ -7,15 +7,19 @@ public class TextureLoader
 {
     public IEnumerator DownloadImage(string MediaUrl, Image image)
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
-        yield return request.SendWebRequest();
-        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
-            Debug.Log(request.error);
-        else
+        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl))
         {
-            Texture2D tex = ((DownloadHandlerTexture)request.downloadHandler).texture;
-            image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(tex.width / 2, tex.height / 2));
-            //Debug.Log("Texture Loaded");
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Texture2D tex = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(tex.width / 2, tex.height / 2));
+                //Debug.Log("Texture Loaded");
+            }
         }
     }
 }
